8f1ad3bd2d17f33d6735f55a05c03d487a4b02c9
swaragade <suraj13jan@gmail.com>
2020-04-09 23:09:08 +0530

    added states

--- src/main/java/com/waragade/coronamh/service/TrendService.java
+++ src/main/java/com/waragade/coronamh/service/TrendService.java
@@ -48,42 +48,81 @@ public class TrendService {
 		stateName.put("andhra pradesh", "Andhra Pradesh");
 		stateName.put("uk", "Uttarakhand");
 		stateName.put("uttarakhand", "Uttarakhand");
+		
 		stateName.put("od", "Odisha");
 		stateName.put("odisha", "Odisha");
+		
 		stateName.put("py", "Puducherry");
 		stateName.put("puducherry", "Puducherry");
+		
 		stateName.put("wb", "West Bengal");
 		stateName.put("west bengal", "West Bengal");
+		
 		stateName.put("ch", "Chandigarh");
 		stateName.put("chandigarh", "Chandigarh");
+		
 		stateName.put("cg", "Chhattisgarh");
 		stateName.put("chhattisgarh", "Chhattisgarh");
+		
 		stateName.put("gj", "Gujarat");
 		stateName.put("gujarat", "Gujarat");
+		
 		stateName.put("hp", "Himachal Pradesh");
 		stateName.put("himachal pradesh", "Himachal Pradesh");
+		
 		stateName.put("mp", "Madhya Pradesh");
 		stateName.put("madhya pradesh", "Madhya Pradesh");
+		
 		stateName.put("br", "Bihar");
 		stateName.put("bihar", "Bihar");
+		
 		stateName.put("mn", "Manipur");
 		stateName.put("manipur", "Manipur");
+		
 		stateName.put("mz", "Mizoram");
 		stateName.put("mizoram", "Mizoram");
+		
 		stateName.put("ga", "Goa");
 		stateName.put("goa", "Goa");
+		
 		stateName.put("an", "Andaman and Nicobar Islands");
 		stateName.put("andaman and nicobar islands", "Andaman and Nicobar Islands");
+		
 		stateName.put("jh", "Jharkhand");
 		stateName.put("jharkhand", "Jharkhand");
+		
 		stateName.put("as", "Assam");
 		stateName.put("assam", "Assam");
+		
 		stateName.put("ar", "Arunachal Pradesh");
 		stateName.put("arunachal pradesh", "Arunachal Pradesh");
+		
 		stateName.put("dn", "Dadra and Nagar Haveli");
 		stateName.put("dadra and nagar haveli", "Dadra and Nagar Haveli");
+		
 		stateName.put("tr", "Tripura");
 		stateName.put("Tripura", "Tripura");
+		
+		stateName.put("tn", "Tamil Nadu");
+		stateName.put("tamil nadu", "Tamil Nadu");
+		
+		stateName.put("sk", "Sikkim");
+		stateName.put("sikkim", "Sikkim");
+		
+		stateName.put("dd", "Daman and Diu");
+		stateName.put("daman and diu", "Daman and Diu");
+		
+		stateName.put("tr", "Tripura");
+		stateName.put("Tripura", "Tripura");
+		
+		stateName.put("ml", "Meghalaya");
+		stateName.put("Meghalaya", "Meghalaya");
+		
+		stateName.put("ld", "Lakshadweep");
+		stateName.put("lakshadweep", "Lakshadweep");
+		
+		stateName.put("nl", "Nagaland");
+		stateName.put("nagaland", "Nagaland");
 	}
 
 	@Autowired
